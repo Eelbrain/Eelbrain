be0f3f4a93296e1fb2e160e2e3d8ab0382bcda4a
Proloy Das <proloy@umd.edu>
2025-05-18 17:39:11 +0530

    MAINT replace urlretrieve by pooch.retrieve

--- eelbrain/datasets/_alice.py
+++ eelbrain/datasets/_alice.py
@@ -1,11 +1,9 @@
 # Helper to download data for Alice example
-import hashlib
-import shutil
-from pathlib import Path
-from urllib.request import urlretrieve
-import zipfile
+import os
+import pooch
 
-from pooch import file_hash
+from pathlib import Path
+import zipfile
 
 from .._types import PathArg
 
@@ -13,28 +11,20 @@ from .._types import PathArg
 def get_alice_path(
         path: PathArg = Path("~/Data/Alice"),
 ):
-    md5 = hashlib.md5()
-
     path = Path(path).expanduser().resolve()
     if path.exists():
         return path
     path.mkdir(exist_ok=True, parents=True)
     urls = [
-        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/stimuli.zip', '4336a47bef7d3e63239c40c0623dc186'],
+        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/stimuli.zip', '92317dbfc81d6aef14fc334abd75d1165cf57501f0c11f8db1a47c76c3d90ac6'],
         # ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.0.zip', 'd63d96a6e5080578dbf71320ddbec0a0'],
-        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.1.zip', 'bdc65f168db4c0f19bb0fed20eae129b'],  # S15-S34
+        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.1.zip', 'a645e4bf30ec8de10c92f82e9f842dd8172a4871f8eb23244e7e78b7dff157aa'],  # S15-S34
         # ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.2.zip', '3fb33ca1c4640c863a71bddd45006815'],
     ]
 
-    for url, hash in urls:
-        temp_file_path, header = urlretrieve(url)
-        # with open(temp_file_path, "rb") as f:
-        #     for chunk in iter(lambda: f.read(1048576), b""):
-        #         md5.update(chunk)
-        # file_hash = md5.hexdigest()
-        # if file_hash != hash:
-        #     raise RuntimeError(f'Hash mismatch for {url}: {file_hash} != {hash}')
+    for url, known_hash in urls:
+        temp_file_path = pooch.retrieve(url, known_hash)
         with zipfile.ZipFile(temp_file_path, 'r') as f:
             f.extractall(path)
-        Path(temp_file_path).unlink()
+        os.remove(temp_file_path)
     return path
