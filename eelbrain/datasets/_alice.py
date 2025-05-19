52c6be2d57f8fbb19f8012c1aa17d5ce1512c3d0
Proloy Das <proloy@umd.edu>
2025-05-19 19:05:50 +0530

    MAINT minor style changes, disable progressbar

--- eelbrain/datasets/_alice.py
+++ eelbrain/datasets/_alice.py
@@ -1,9 +1,7 @@
 # Helper to download data for Alice example
-import os
-import pooch
-
 from pathlib import Path
-import zipfile
+
+import pooch
 
 from .._types import PathArg
 
@@ -14,17 +12,7 @@ def get_alice_path(
     path = Path(path).expanduser().resolve()
     if not path.exists():
         path.mkdir(exist_ok=True, parents=True)
-    urls = [
-        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/stimuli.zip', '92317dbfc81d6aef14fc334abd75d1165cf57501f0c11f8db1a47c76c3d90ac6'],
-        # ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.0.zip', 'd63d96a6e5080578dbf71320ddbec0a0'],
-        ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.1.zip', 'a645e4bf30ec8de10c92f82e9f842dd8172a4871f8eb23244e7e78b7dff157aa'],  # S15-S34
-        # ['https://drum.lib.umd.edu/bitstream/handle/1903/27591/eeg.2.zip', '3fb33ca1c4640c863a71bddd45006815'],
-    ]
-    # for url, known_hash in urls:
-        # temp_file_path = pooch.retrieve(url, known_hash, progressbar=True)
-        # with zipfile.ZipFile(temp_file_path, 'r') as f:
-        #     f.extractall(path)
-        # os.remove(temp_file_path)
+
     baseurl = 'https://drum.lib.umd.edu/bitstream/handle/1903/27591/'
     registry = {
         'stimuli.zip': '92317dbfc81d6aef14fc334abd75d1165cf57501f0c11f8db1a47c76c3d90ac6',
@@ -37,6 +25,8 @@ def get_alice_path(
         retry_if_failed=4,
     )
     for fname in registry.keys():
-        fetcher.fetch(fname, progressbar=True, processor=pooch.Unzip(extract_dir='.'))
-        os.remove(path / fname)
+        if (path / fname.split('.')[0]).exists():   # Won't work for multiple eeg.x.zip download
+            continue
+        fetcher.fetch(fname, processor=pooch.Unzip(extract_dir='.'))
+        (path / fname).unlink()
     return path
