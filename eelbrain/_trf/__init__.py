064fcb60a1bd813cb00e79d7844f8c94a098f8d2
Yanyu Woo <wuyanyu0921@outlook.com>
2026-05-26 16:36:39 -0400

    feat: add export module raw

---
+++ eelbrain/_trf/__init__.py
@@ -0,0 +1,6 @@
+from ._code import Code
+from ._experiment import TRFExperiment
+from ._model import Term
+from ._predictor import EventPredictor, FilePredictor, MakePredictor, SessionPredictor
+from ._results import ResultCollection
+from ._estimator import Estimator, BoostingEstimator, NCRFEstimator
