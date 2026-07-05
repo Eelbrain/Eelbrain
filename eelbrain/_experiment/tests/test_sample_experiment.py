ab67033e1d6461ae302eaa7d34170bf02f442c91
Christian Brodbeck <christianbrodbeck@nyu.edu>
2026-07-05 10:22:43 -0400

    AMD always interpolate bad segments

--- eelbrain/_experiment/tests/test_sample_experiment.py
+++ eelbrain/_experiment/tests/test_sample_experiment.py
@@ -1031,8 +1031,8 @@ def test_channel_model_rejection_variable_length(samples_experiment):
 
     # end-to-end: interpolation runs and only touches samples inside the windows
     max_interpolate = rej_ds.info[INTERPOLATE_WINDOWS_MAX]
-    ds0 = e.load_epochs(interpolate_bads=False, baseline=False)
-    ds1 = e.load_epochs(interpolate_bads=True, baseline=False)
+    ds0 = e.load_epochs(interpolate_bads=True, baseline=False, epoch_rejection='')
+    ds1 = e.load_epochs(interpolate_bads=True, baseline=False, epoch_rejection='auto')
     assert isinstance(ds1['eeg'], Datalist)
     assert len(ds1['eeg']) == len(windows)
     changed = zeroed_any = False
