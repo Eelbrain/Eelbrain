2819374e0414e832c33ad836b6acf27b2dc6dc30
Christian Brodbeck <christianbrodbeck@nyu.edu>
2026-08-21 14:06:18 -0400

    fix

--- eelbrain/_trf/_boosting.py
+++ eelbrain/_trf/_boosting.py
@@ -507,14 +507,8 @@ class BoostingResult(PickleableDataClass):
                 y_pred += self.y_mean
         return y_pred
 
-    def partition_result_data(self, model: str = None) -> Dataset:
-        """Results from the different test partitions in a :class:`Dataset`
-
-        Parameters
-        ----------
-        model
-            Add a ``'model'`` column to the dataset to distinguish
-        """
+    def partition_result_data(self) -> Dataset:
+        """Results from the different test partitions in a :class:`Dataset`"""
         h_is_list = isinstance(self._h, tuple)
         rows = []
         for res in self.partition_results:
